b900d03e5c522243b57759b54d7aef595f77ba39
Daniel Eduardo da Silva Cunha <daniel.cunha@sou.inteli.edu.br>
2022-06-20 09:16:24 -0300

    Atualizacoes

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Pagina Html/backend/server.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Pagina Html/backend/server.js
@@ -4,6 +4,8 @@ const port = 3055
 const sqlite3 = require("sqlite3").verbose()
 const sqlite = require("sqlite")
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(express.static("../frontend/"));
 app.get("/", (req, res) => {
     res.render("index.html")
@@ -17,6 +19,27 @@ app.get("/curriculo", (req, res) => {
         res.status(200).json(result)
     }
     getdb()
+})
+app.post("/inserir", (req, res) => {
+    async function getNome() {
+    const db = await sqlite.open({ filename: "./bancodeDados.db", driver: sqlite3.Database })
+    const result = await db.run(`INSERT INTO userInfo (Nome) VALUES (?)`,[req.body.nome])
+    
+    }
+    getNome()
+    res.status(200).send()
+
+
+})
+app.put("/atualizar", (req,res) => {
+    async function getUpdate() {
+        const db = await sqlite.open({ filename: "./bancodeDados.db", driver: sqlite3.Database })
+        const result = await db.run(`UPDATE userInfo SET numero = ? WHERE id = 1`, [req.body.numero,])
+
+    }
+    getUpdate()
+    res.status(200).send()
+
 })
 app.listen(port, () => {
     console.log("o seu servidor está rodando http://localhost:3055")
