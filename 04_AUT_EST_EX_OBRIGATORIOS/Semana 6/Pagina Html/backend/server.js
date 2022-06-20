b1fecba750a1c8d9e60a950cfca7d5ff63ab33b2
Daniel Eduardo da Silva Cunha <daniel.cunha@sou.inteli.edu.br>
2022-06-20 08:35:30 -0300

    Atualizacoes

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 6/Pagina Html/backend/server.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 6/Pagina Html/backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express")
 const app = express()
 const port = 3055
+const sqlite3 = require("sqlite3").verbose()
+const sqlite = require("sqlite")
+
 app.use(express.static("../frontend/"));
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index.html")
 })
+app.get("/curriculo", (req, res) => {
+
+    async function getdb() {
+        const db = await sqlite.open({ filename: "./bancodeDados.db", driver: sqlite3.Database })
+        console.log(db)
+        const result = await db.get("SELECT * FROM userInfo")
+        res.status(200).json(result)
+    }
+    getdb()
+})
 app.listen(port, () => {
-console.log("o seu servidor está rodando http://localhost:3055")
+    console.log("o seu servidor está rodando http://localhost:3055")
 })
 
 
--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 6/Pagina Html/frontend/alert.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 6/Pagina Html/frontend/alert.js
@@ -2,3 +2,13 @@ $( document ).ready(function() {
     alert("Essa mensagem foi escrita utilizando o .ready do jquery")
 
 });
+function getDados() {
+    $.ajax({
+        url:"http://localhost:3055/curriculo",
+        method: "GET",
+        success: function (res){
+            $("#nomeDb").html(res.Nome)
+            console.log(res.Nome)
+        }
+    })
+}
